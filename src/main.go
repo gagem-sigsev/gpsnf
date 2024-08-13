424abafec899d9c32d2cec59690b9f69de0de5b4
Gage-Morgan <gagemorgan206@gmail.com>
2024-08-12 21:50:17 -0500

    add command line argument processing

--- src/main.go
+++ src/main.go
@@ -3,10 +3,27 @@ package main
 import (
 	"fmt"
 	"log"
+  "flag"
+  "os"
 )
 
 func main() {
-	handle, err := openDev("wlan0")
+
+  // Define a string flag with a default value and a usage description.
+  interfaceFlag := flag.String("interface", "", "Specify the network interface name")
+
+  // Parse the command-line flags.
+  flag.Parse()
+
+  // Check if the required flag is provided.
+  if *interfaceFlag == "" {
+    // Print usage and exit if the flag is not provided.
+    fmt.Println("Usage: ./progName --interface <interface name>")
+    flag.PrintDefaults()
+    os.Exit(1)
+  }
+
+	handle, err := openDev(*interfaceFlag)
 	if err != nil {
 		log.Fatal(err)
 	}
